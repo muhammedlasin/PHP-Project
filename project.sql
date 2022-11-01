c2e366083c858ab2124666148b5ae089ce1cf554
Navaneeth <navaneeth.m@terrificminds.com>
2022-11-01 11:36:40 +0530

    Added TASKS table

---
+++ project.sql
@@ -0,0 +1,16 @@
+CREATE TABLE TASKS (
+   task_id INT NOT NULL AUTO_INCREMENT,
+   project_id INT, NOT NULL, 
+   task_name VARCHAR(255) NOT NULL, 
+   task_description TEXT NOT NULL, 
+   status VARCHAR(64) NOT NULL DEFAULT 'Open',
+   developer_id INT NOT NULL, 
+   priority VARCHAR(64) NOT NULL DEFAULT 'Low',
+   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+   created_by VARCHAR(64) NOT NULL,
+   updated_by VARCHAR(64) NOT NULL,
+   PRIMARY KEY (task_id),
+   FOREIGN KEY(project_id) REFERENCES PROJECTS(project_id)
+);
+
