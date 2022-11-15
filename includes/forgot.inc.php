17d504f86a186087dfa33f3f97dda8ce30a14e00
Christina Antony <christina.antony@terrificminds.com>
2022-11-15 09:58:51 +0530

    merged

--- includes/forgot.inc.php
+++ includes/forgot.inc.php
@@ -3,18 +3,19 @@
 //include "../sendmail.php";
 
 if (isset($_POST["forgot"])) {
-    echo "reached";
+ 
     $email=$_POST["email"];
 
 
     include "../classes/Dbh.class.php";
     include "../classes/Forgot.classes.php";
     include "../classes/ForgotContr.classes.php";
-
+ 
 
     $forgot = new ForgotContr();
 
     $forgot-> forgotUser($email);
+    
     
     $subject="Reset password";
     $message=" Please click this link to reset your password : http://localhost/PHP-Project/reset.php?email=$email ";
